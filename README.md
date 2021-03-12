c4df5eff5393d3bf3d6ea30cb0bc07bd23e49c81
Josemar Lohn <j@lo.hn>
2021-03-12 11:18:49 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# urlencode
+
+encode URL data
+
+# Install
+
+Available as a [bpkg](http://www.bpkg.sh/)
+```sh
+bpkg install [-g] lohn/urlencode
+```
---
+++ urlencode.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+VERSION=0.0.1
+
+usage () {
+  echo "urlencode [-hV]"
+  echo
+  echo "Options:"
+  echo "  -h|--help      Print this help dialogue and exit"
+  echo "  -V|--version   Print the current version and exit"
+}
+
+urlencode () {
+  for opt in "${@}"; do
+    case "$opt" in
+      -h|--help)
+        usage
+        return 0
+        ;;
+      -V|--version)
+        echo "$VERSION"
+        return 0
+        ;;
+    esac
+  done
+
+  ## your code here
+}
+
+if [[ ${BASH_SOURCE[0]} != $0 ]]; then
+  export -f urlencode
+else
+  urlencode "${@}"
+  exit $?
+fi
